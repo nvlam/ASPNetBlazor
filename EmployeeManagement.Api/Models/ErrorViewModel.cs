1e9457789449de854d888acc06fa3853aa6dab0f
nvlam <nvlam.ensea@gmail.com>
2021-10-09 11:43:04 +0700

    nvlam: Asp.Net Blaor, Models, API, Add-Migration, Repository

---
+++ EmployeeManagement.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EmployeeManagement.Web/EmployeeListBase.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Components;
+using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web
+{
+    public class EmployeeListBase:ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+           await Task.Run(LoadEmployees);
+           // return base.OnInitializedAsync();
+        }
+
+        private void LoadEmployees()
+        {
+            System.Threading.Thread.Sleep(5000);
+            Employee e1 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "John",
+                LastName = "Hastings",
+                Email = "David@pragimtech.com",
+                DateOfBirth = new DateTime(1980, 10, 5),
+                Gender = Gender.Male,
+                //  Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 1,
+                PhotoPath = "images/john.png"
+            };
+
+        Employee e2 = new Employee
+        {
+            EmployeeId = 2,
+            FirstName = "Sam",
+            LastName = "Galloway",
+            Email = "Sam@pragimtech.com",
+            DateOfBirth = new DateTime(1981, 12, 22),
+            Gender = Gender.Male,
+            //   Department = new Department { DepartmentId = 2, DepartmentName = "HR" },
+            DepartmentId = 2,
+                PhotoPath = "images/sam.jpg"
+            };
+
+            Employee e3 = new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Mary",
+                LastName = "Smith",
+                Email = "mary@pragimtech.com",
+                DateOfBirth = new DateTime(1979, 11, 11),
+                Gender = Gender.Female,
+                // Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 2,
+            PhotoPath = "images/mary.png"
+            };
+
+            Employee e4 = new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Sara",
+                LastName = "Longway",
+                Email = "sara@pragimtech.com",
+                DateOfBirth = new DateTime(1982, 9, 23),
+                Gender = Gender.Female,
+                // Department = new Department { DepartmentId = 3, DepartmentName = "Payroll" },
+                DepartmentId = 2,
+            PhotoPath = "images/sara.png"
+            };
+
+            Employees = new List<Employee> { e1, e2, e3, e4 };
+        }
+    }
+}
