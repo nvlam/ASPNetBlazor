ca42e125ee013b1c1ab14dab0372df0d0f352624
nvlam <nvlam.ensea@gmail.com>
2021-10-11 14:44:14 +0700

    nvlam: List of Employees api

---
+++ EmployeeManagement.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeManagement.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository employeeRepository;
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetEmployees()
+        {
+            try { return Ok(await employeeRepository.GetEmployees()); 
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from database");
+            }
+
+           
+        }
+
+    }
+}
