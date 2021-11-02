3732265cb87a69bd605f0239360a67f84fc37c27
nvlam <nvlam.ensea@gmail.com>
2021-11-02 16:02:05 +0700

    nvlam: Custom form validation with Blazor

---
+++ EmployeeManagement.Models/CustomValidators/EmailDomainValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models.CustomValidators
+{
+    class EmailDomainValidator : ValidationAttribute
+    {
+        public string AllowedDomain { get; set; }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string[] strings = value.ToString().Split('@');
+            if (strings[1].ToUpper() == AllowedDomain.ToUpper())
+            {
+                return null;
+            }
+
+            return new ValidationResult(ErrorMessage,
+                new[] { validationContext.MemberName });
+        }
+    }
+}
--- EmployeeManagement.Models/Employee.cs
+++ EmployeeManagement.Models/Employee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManagement.Models.CustomValidators;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,18 +8,19 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagement.Models
 {
-   public class Employee
+    public class Employee
     {
         public int EmployeeId { get; set; }
 
-       [Required(ErrorMessage ="Tên bắt buộc phải nhập")]
-       [StringLength(100, MinimumLength =2,ErrorMessage ="Tên tối đa 100 ký tự, tối thiểu 2 ký tự")]
+        [Required(ErrorMessage = "Tên bắt buộc phải nhập")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Tên tối đa 100 ký tự, tối thiểu 2 ký tự")]
         public string FirstName { get; set; }
 
-       [Required(ErrorMessage = "Họ bắt buộc phải nhập")]
+        [Required(ErrorMessage = "Họ bắt buộc phải nhập")]
         public string LastName { get; set; }
 
-       [EmailAddress(ErrorMessage ="Nhập email phải đúng định dạng")]
+        
+        [EmailDomainValidator(AllowedDomain = "kgc.edu.vn", ErrorMessage = "Chi nhan email tu kgc.edu.vn")]           
         public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Gender Gender { get; set; }
